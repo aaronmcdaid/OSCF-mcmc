7c734ecf736a03e9d4d63a34983c403636eae215
Aaron McDaid <aaronmcdaid@gmail.com>
2012-11-05 17:05:53 +0000

    Store a sorted edge list in EdgeSet.

--- vcsbm.cpp
+++ vcsbm.cpp
@@ -8,6 +8,9 @@ gengetopt_args_info args_info; // a global variable! Sorry.
 
 #include "network.hpp"
 
+#include<algorithm>
+#include<cassert>
+
 using namespace std;
 
 int main(int argc, char **argv) {
@@ -48,4 +51,33 @@ int main(int argc, char **argv) {
 		cout << left_str << ' ' << right_str << endl;
 	}
 #endif
+	// Finally, we create the list of Endpoints - by doubling the list of Edges
+	struct EndPoint {
+		int edge_id; // edge_id: a number between 0 and E
+		int endpoint_type; // 1: I'm the source, and the other end of the sink
+		                   // -1: Other way around
+				   // 0: Undirected, so it doesn't matter.
+		int this_node_id; // id of this node at this end of the edge.
+		int far_node_id; // id of the node at the other end of this  edge.
+		EndPoint(int edge_id_, int endpoint_type_, int this_node_id_, int far_node_id_)
+			: edge_id(edge_id_), endpoint_type(endpoint_type_), this_node_id(this_node_id_), far_node_id(far_node_id_) {}
+		bool operator <(const EndPoint &other) const {
+			if(this->this_node_id < other.this_node_id) return true;
+			if(this->this_node_id > other.this_node_id) return false;
+			assert(this->this_node_id == other.this_node_id);
+			if(this->far_node_id < other.far_node_id) return true;
+			if(this->far_node_id > other.far_node_id) return false;
+			assert(this->far_node_id == other.far_node_id);
+			return false;
+		}
+	};
+	vector<EndPoint> all_endpoints_sorted; // we'll sort this later
+	for(int e=0; e<E; ++e) {
+		const network :: EdgeSet :: Edge & edge = edge_set->edges.at(e);
+		EndPoint l(e, args_info.directed_flag ? 1 : 0, edge.left, edge.right);
+		EndPoint r(e, args_info.directed_flag ?-1 : 0, edge.right, edge.left);
+		all_endpoints_sorted.push_back(l);
+		all_endpoints_sorted.push_back(r);
+	}
+	sort(all_endpoints_sorted.begin(), all_endpoints_sorted.end());
 }
