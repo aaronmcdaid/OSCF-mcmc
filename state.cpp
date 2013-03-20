b94ca220c358f137bc90f9db878797b863aecd15
Aaron McDaid <aaronmcdaid@gmail.com>
2013-03-20 11:54:13 +0000

    Initialize with every node in its *own* community

--- state.cpp
+++ state.cpp
@@ -1,3 +1,9 @@
 #include "state.hpp"
-State :: State(Net net_) : net(net_) {
+State :: State(Net net_) : net(net_), E(net_->E()), N(net_->N()) {
+	this->K = 0;
+	// initially, there are no communities, and no edges are in any community.
+	// But we'll initialize by placing each each in a community of its own.
+	for(int64_t e = 0; e < E; ++e) {
+		this->comms.at(e).add_edge(e, net);
+	}
 }
--- state.hpp
+++ state.hpp
@@ -19,11 +19,61 @@
  * OR
  * - a map: edge_id -> set(community_id)  (A non-empty set)
  *
+ *
+ * Finally, I will (sort of) allow "missing data" - some edges might be totally unassigned at certain times.
+ *
  */
+#include <vector>
+#include <cassert>
+#include "tr1/unordered_set"
+
 #include "network.hpp"
 
-typedef const network :: Network * const Net;
+struct Community {
+private:
+	std :: tr1 :: unordered_set<int64_t> my_edges;
+	std :: tr1 :: unordered_multiset<int64_t> my_nodes; // each node can appear multiple times
+	int64_t unique_nodes_in_this_community;
+public:
+	Community() : unique_nodes_in_this_community(0) {}
+	void add_edge(int64_t e, Net net) {
+		const bool inserted = this->my_edges.insert(e).second;
+		assert(inserted);
+		const network :: EdgeSet :: Edge & edge = net->edge_set->edges.at(e);
+		this->add_node(edge.left);
+		if(edge.left!=edge.right)
+			this->add_node(edge.right);
+	}
+	void add_node(int64_t n) {
+		this->my_nodes.insert(n);
+		const size_t how_many = this->my_nodes.count(n);
+		assert(how_many>0);
+		if(how_many == 1) { // we've just added this new unique node
+			++ this->unique_nodes_in_this_community;
+		}
+	}
+};
+
+struct Communities {
+	// This is just a vector<Community> that will grow, and maybe shrink, every now and then.
+private:
+	std :: vector<Community> comms;
+public:
+	Community & at(const int64_t k) {
+		if(k >= this->comms.size())
+			this->comms.resize(k+1);
+		return this->comms.at(k);
+	}
+};
+
 struct State {
 	Net net;
+	const int64_t N;
+	const int64_t E;
+
+	int64_t K; // number of communities
+	Communities comms;
+
+
 	explicit State(Net net_);
 };
